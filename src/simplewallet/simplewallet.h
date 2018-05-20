9f2ea08a8ec59ffa6ec606dbc2696c3cdaa16dde
wowario <wowario@protonmail.com>
2018-05-20 21:56:40 +0300

    make simplewallet simple

--- src/simplewallet/simplewallet.h
+++ src/simplewallet/simplewallet.h
@@ -144,6 +144,7 @@ namespace cryptonote
     bool set_ignore_fractional_outputs(const std::vector<std::string> &args = std::vector<std::string>());
     bool set_device_name(const std::vector<std::string> &args = std::vector<std::string>());
     bool help(const std::vector<std::string> &args = std::vector<std::string>());
+    bool help_advanced(const std::vector<std::string> &args = std::vector<std::string>());
     bool start_mining(const std::vector<std::string> &args);
     bool stop_mining(const std::vector<std::string> &args);
     bool set_daemon(const std::vector<std::string> &args);
