d55d97ac697e2f8f4df396337e28cb76d8bf9c55
wowario <wowario@protonmail.com>
2019-11-08 18:16:38 +0300

    update checkpoints.dat

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5091,7 +5091,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "b02311ef2ce826fec8a24a5b8f736e4c12d6b0a66434d915bf0ba34740fe84ac";
+static const char expected_block_hashes_hash[] = "8bc124ea71929ed6fcb886c056ac26d789f15884c1726224485835cec932b259";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
